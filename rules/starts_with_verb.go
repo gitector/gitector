e3981104bc15baed667619fc82043bf9be1beb57
Michał Koźmiski <michal@kozminski.co>
2020-04-03 19:59:45 +0200

    Add check against ed/ing + irregular verbs

--- rules/starts_with_verb.go
+++ rules/starts_with_verb.go
@@ -1,14 +1,30 @@
 package rules
 
 import (
-	"fmt"
 	"github.com/gobuffalo/packr"
+	"gitlab.com/tbacompany/gitector/utils"
 	"strings"
 )
 
 var wordsBox = packr.NewBox("./words")
 
-func StartsWithVerb(title string) bool {
+func StartsWithBaseVerb(title string) bool {
+	firstWord := utils.ExtractFirstWord(title)
+
+	if endsWithEdOrIng(firstWord) || oneOfIrregularVerbs(firstWord) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func oneOfIrregularVerbs(word string) bool {
 	irregularVerbs, _ := wordsBox.FindString("irregular_verbs.txt")
 	irregularVerbsList := strings.Split(irregularVerbs, "\n")
+
+	return utils.Contains(word, irregularVerbsList)
+}
+
+func endsWithEdOrIng(word string) bool {
+	return strings.HasSuffix(word, "ed") || strings.HasSuffix(word, "ing")
 }
--- rules/starts_with_verb_test.go
+++ rules/starts_with_verb_test.go
@@ -6,9 +6,13 @@ import (
 )
 
 func TestChecksForBaseForm(t *testing.T) {
-	assert.True(t, StartsWithVerb("Add new test"))
+	assert.True(t, StartsWithBaseVerb("Add new test"))
 }
 
 func TestFailsIfPastTenseUsed(t *testing.T) {
-	assert.False(t, StartsWithVerb("Added new test"))
+	assert.False(t, StartsWithBaseVerb("Added new test"))
+}
+
+func TestFailsForIrregular(t *testing.T) {
+	assert.False(t, StartsWithBaseVerb("took new actions against further faliures"))
 }
